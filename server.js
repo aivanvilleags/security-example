50f1e189fb13fea075cb4393d71a20b2ab3fd3d6
aivan <haivan.villegas@gmail.com>
2022-04-05 11:50:38 -0500

    added ssl certificate

---
+++ server.js
@@ -0,0 +1,28 @@
+const fs = require("fs");
+const path = require("path");
+const https = require("https");
+
+const express = require("express");
+const PORT = 4000;
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+app.get("/secret", (req, res) => {
+  res.send("Secret number: 44");
+});
+
+https
+  .createServer(
+    {
+      key: fs.readFileSync("key.pem"),
+      cert: fs.readFileSync("cert.pem"),
+    },
+    app
+  )
+  .listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+  });
